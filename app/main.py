f8b40951ce78b3e5f588dd8fd05af3ecf4d4e534
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 21:55:26 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ def copy_file(command: str) -> None:
     c_files_list = command.split()
     source_file, destination_file = c_files_list[1], c_files_list[2]
     if source_file != destination_file:
-        with (open(source_file, "r") as file_in,
-              open(destination_file, "w") as file_out):
-            file_out.write(file_in.read())
+        try:
+            with (open(source_file, "r") as file_in,
+                  open(destination_file, "w") as file_out):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            print(f"File {source_file} was not found in the directory.")
