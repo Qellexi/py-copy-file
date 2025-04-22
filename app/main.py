15f31a76a21d05542f2ba21f573ab8fff46b325c
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 21:47:54 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 def copy_file(command: str) -> None:
-    c_files_list = (command.split("cp "))[1].split(" ")
-    source_file = c_files_list[0]
-    destination_file = c_files_list[1]
+    if not command.startswith("cp"):
+        raise ValueError("Incorrect command format.")
+    c_files_list = command.split()
+    source_file, destination_file = c_files_list[1], c_files_list[2]
     if source_file != destination_file:
-        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
-
