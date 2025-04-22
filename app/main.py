c71128efe3646c324217c9cd113e790451594dca
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 22:01:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
     if not command.startswith("cp"):
-        raise ValueError("Incorrect command format.")
-    c_files_list = command.split()
-    source_file, destination_file = c_files_list[1], c_files_list[2]
-    if source_file != destination_file:
-        try:
-            with (open(source_file, "r") as file_in,
-                  open(destination_file, "w") as file_out):
-                file_out.write(file_in.read())
-        except FileNotFoundError:
-            print(f"File {source_file} was not found in the directory.")
+        c_files_list = command.split()
+        source_file, destination_file = c_files_list[1], c_files_list[2]
+        if source_file != destination_file:
+            try:
+                with (open(source_file, "r") as file_in,
+                      open(destination_file, "w") as file_out):
+                    file_out.write(file_in.read())
+                    print(f"File {source_file} was successfully copied to {destination_file}.")
+            except FileNotFoundError:
+                print(f"File {source_file} was not found in the directory.")
