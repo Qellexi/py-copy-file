aabb18b8c712e14615db81643bb18bfd7a893af3
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 21:42:30 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def copy_file(command: str) -> None:
     c_files_list = (command.split("cp "))[1].split(" ")
-    file = c_files_list[0]
-    new_file = c_files_list[1]
-    if file != new_file:
-        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+    source_file = c_files_list[0]
+    destination_file = c_files_list[1]
+    if source_file != destination_file:
+        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
             file_out.write(file_in.read())
+
